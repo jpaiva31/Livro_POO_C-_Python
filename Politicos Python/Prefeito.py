4e9a1b56496dd90bd7577eace48e5aefa458f4df
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-09-19 19:57:30 -0300

    Update Prefeito.py

--- Politicos Python/Prefeito.py
+++ Politicos Python/Prefeito.py
@@ -7,10 +7,11 @@ class Prefeito(Politico):
         self.set_Municipio(municipio)
         self.setEstado(estado)
         self.setFuncao("Administrar o IPTU visando o melhor para a cidade")
+        self.setSalario(1000)
     def set_Municipio(self,municipio):
         self.municipio = municipio
     def get_Municipio(self):
         return self.municipio
     def apresentacao(self):
         Politico.apresentacao(self)
-        print(f'Sou prefeito em {self.get_Municipio()}/{self.getEstado}')
+        print(f'Sou prefeito em {self.get_Municipio()}/{self.getEstado()} e meu suposto salário é R$ {self.getSalario()}')
